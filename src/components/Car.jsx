fe948e492041ae59b7fbcbdef825a80b5826fa02
Warner Moscoso Morales <125229121+WaCnerDev@users.noreply.github.com>
2025-03-20 17:06:14 -0600

    Add Footer component and styles; update Car component layout

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -5,19 +5,21 @@ import { ShoppingPage } from "../pages/ShoppingPage";
 import { ShoppingCartPage } from "../pages/ShoppingCartPage";
 import { ProductProvider } from "../context/ProductProvider";
 import { ShoppingCartProvider } from "../context/ShoppingCartProvider";
+import { Footer } from "./Footer";
 
 export const Car = () => {
   return (
     <ProductProvider>
       <ShoppingCartProvider>
         <NavBar />
-        <div className="container">
+        <div className="container py-5">
           <Routes>
             <Route path="/" element={<ShoppingPage />} />
             <Route path="/shopCart" element={<ShoppingCartPage />} />
             <Route path="/*" element={<Navigate to="/" />} />
           </Routes>
         </div>
+        <Footer />
       </ShoppingCartProvider>
     </ProductProvider>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+export const Footer = () => {
+  return (
+    <footer>
+      <p>© 2025 SHOPPKUA. Todos los derechos reservados.</p>
+    </footer>
+  )
+}
