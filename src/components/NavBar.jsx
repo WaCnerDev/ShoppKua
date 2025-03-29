513c56e81afd64c37c19c57595ccf5b93c361073
Warner Moscoso Morales <125229121+WaCnerDev@users.noreply.github.com>
2025-03-28 20:58:34 -0600

    Feat: Update NavBar component; add new products link and improve styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,38 +6,38 @@ export const NavBar = () => {
     <nav className="navbar navbar-expand-lg border-bottom bg-light shadow-sm">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-center w-100 gap-4">
         <li className="nav-item">
-          <NavLink to="/" className="nav-link active">
+          <NavLink to="/" className="nav-link">
             Clothes
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/" className="nav-link active">
+          <NavLink to="/" className="nav-link">
             Accessories
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/" className="nav-link active">
-            Electronics
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/" className="nav-link active">
+          <NavLink to="/" className="nav-link">
             Footwear
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/" className="nav-link active">
-            Deport
+          <NavLink to="/" className="nav-link">
+            Electronics
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/" className="nav-link active">
+          <NavLink to="/" className="nav-link">
             Home
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/" className="nav-link active">
+          <NavLink to="/" className="nav-link">
             Oferts
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/" className="nav-link">
+            New Products
           </NavLink>
         </li>
       </ul>
